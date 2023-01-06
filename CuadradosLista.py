c23fe488426ab111c7560dcbcd63fa459e040dde
Sebastián Diaz <99108952+SebasMas@users.noreply.github.com>
2023-01-06 16:40:19 -0500

    EasyProgram

---
+++ CuadradosLista.py
@@ -0,0 +1,11 @@
+def cuadrados(numeros):
+    Newlist = []
+    for i in numeros:
+       cuadrado=  i**2
+       Newlist.append(cuadrado)
+    return print(Newlist)
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(cuadrados(numeros))
+    
+
